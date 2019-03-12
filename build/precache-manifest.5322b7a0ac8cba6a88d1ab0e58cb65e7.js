d08e94a65e95bdae45b1e1d86e887e3d65baa4cb
chuluuu <chuluu_zero@yahoo.com>
2019-03-12 18:00:11 +0800

    add files

---
+++ build/precache-manifest.5322b7a0ac8cba6a88d1ab0e58cb65e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ce8e95abcb0c611fc546",
+    "url": "/static/js/main.e20023b4.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "ce8e95abcb0c611fc546",
+    "url": "/static/css/main.8f7e1529.chunk.css"
+  },
+  {
+    "revision": "cf91461d98780941567775954001a583",
+    "url": "/index.html"
+  }
+];
