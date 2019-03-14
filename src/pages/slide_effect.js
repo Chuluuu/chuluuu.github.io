b0f9e49dbccdc60cadbbfb651ebb1aed68786b15
chuluuu <chuluu_zero@yahoo.com>
2019-03-14 18:39:51 +0800

    try

--- src/pages/slide_effect.js
+++ src/pages/slide_effect.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
-import {maldive} from '../images/maldive.jpg';
+//import {maldive} from '../images/maldive.jpg';
 import './slide.css';
  
 const slideImages = [
-  '../images/maldive.jpg',
-  '../images/maldive1.jpg',
-  '../images/maldive2.jpg',
+  '/images/maldive.jpg',
+  '/images/maldive1.jpg',
+  '/images/maldive2.jpg',
   // '../images/maldive3.jpg',
   // '../images/maldive4.jpg',
   // '../images/maldive5.jpg',
   // '../images/maldive6.jpg',
   // '../images/maldive7.jpg',
 ];
+
  
 const properties = {
   duration: 2000,
@@ -23,25 +24,21 @@ const properties = {
 }
 //  console.log('TEEE'+maldive)
 const Slideshow = () => {
+  const load_image = slideImages.map(images => (
+    <div className="each-slide">    
+    {images}
+    <div style={{'backgroundImage': `url(` + images + `)`}}>
+      <span>Slide 1</span>
+    </div>
+    </div> 
+    ));
+    
     return (
-      <Slide {...properties}>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${maldive})`}}>
-            <span>Slide 1</span>
-          </div>
+      // <Slide {...properties}> 
+      <div>   
+        <img src='/images/maldive2.jpg' width="50px"></img>
         </div>
-        <div className="each-slide">
-          <div className='trees__tree' >
-            <span>Slide 2</span>
-          </div>
-        </div>
-        {/* <div className="each-slide">
-          <div style={{'backgroundImage': 'url(require('${slideImages[2]})+')' }}>
-            <span>Slide 3</span>
-          </div>
-        </div> */}
-        
-      </Slide>
+      // </Slide>
     )
 }
 
