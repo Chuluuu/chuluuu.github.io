f191b33d5cf03dcfc2e3de665a53bd2198e9db2f
chuluuu <chuluu_zero@yahoo.com>
2019-05-19 20:14:59 +0800

    [ADD]

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,43 +3,46 @@ import React, { Component } from 'react';
 class about extends Component {
     render() {
         return (
-            <div>
-               <div className="about">
-          <div className="intro">
-            <div className="intro-description">
-              <p>
-                <span>
-                  Work.
-                </span>
-                For some time now, I've been working closely with engineers, first as a Front-end Engineer and Designer, then as a Tech Lead and now as an Engineering Manager. I've spent the last 5 years at a startup where I joined as employee #3 (they've since raised several rounds and are at 135+ employees).
-              </p>
-              <p>
-                When coding, my goal is to implement and hand off compliant and pixel-perfect templates for engineers to flesh out with backend functionality. When designing, my tools of choice are Photoshop and Invision. When recruiting, I spend time vetting engineers, conducting initial phone screens and in-house interviews—I then manage many of these new hires. As a tech lead, I focused on delivering features for our consumer facing web application, working closely with product managers and various stakeholders across the company. Now, I focus primarily on managing a team of fullstack engineers.
-              </p>
-            </div>
-            <div className="intro-description">
-              <p>
-                <span>
-                  Fun.
-                </span>
-                I enjoy interior design, and am obsessed with figuring out how to make small spaces beautiful and functional. I have perfected the art of home-made waffles and I'm a big fan of Bea Arthur ice cream cone from Big Gay Ice Cream Shop. But my favorite way to spend time is to relax and cuddle with my clingy cat Toby and my hyperactive dog Butters while binge watching TV shows with
-                <a href="http://www.ianenders.com" rel="noopener noreferrer" target="_blank">
-                  Ian.
-                </a>
-                I've tried my hand at gardening but I'm not quite there yet—my ferns have seen better days. I dislike the entire winter and summer seasons, waking up early, and IPAs.
-              </p>
-              <p>
-                I studied music performance at Carnegie Mellon University, then took up wood working, began a Masters in Design + Technology at Parsons, and now I lead and manage full-stack engineers. I keep busy and I get things done. Check out
-                <a href="/Ana_Enders_Resume.pdf" rel="noopener noreferrer" target="_blank">my resume</a>
-                to learn more about what I do.
-              </p>
-            </div>
-          </div>
-
             
-          
-        </div> 
-            </div>
+              <div className="about">
+                <div className="intro">
+                  <div className="intro-description">
+                    <p>
+                      <span>
+                        Work.
+                      </span>
+                      For some time now, I've been working closely with engineers, first as a Front-end Engineer and Designer, then as a Tech Lead and now as an Engineering Manager. I've spent the last 5 years at a startup where I joined as employee #3 (they've since raised several rounds and are at 135+ employees).
+                    </p>
+                    <p>
+                      When coding, my goal is to implement and hand off compliant and pixel-perfect templates for engineers to flesh out with backend functionality. When designing, my tools of choice are Photoshop and Invision. When recruiting, I spend time vetting engineers, conducting initial phone screens and in-house interviews—I then manage many of these new hires. As a tech lead, I focused on delivering features for our consumer facing web application, working closely with product managers and various stakeholders across the company. Now, I focus primarily on managing a team of fullstack engineers.
+                    </p>
+                  </div>
+                  <div className="intro-description">
+                    <p>
+                      <span>
+                        Fun.
+                      </span>
+                      I enjoy interior design, and am obsessed with figuring out how to make small spaces beautiful and functional. I have perfected the art of home-made waffles and I'm a big fan of Bea Arthur ice cream cone from Big Gay Ice Cream Shop. But my favorite way to spend time is to relax and cuddle with my clingy cat Toby and my hyperactive dog Butters while binge watching TV shows with
+                      <a href="http://www.ianenders.com" rel="noopener noreferrer" target="_blank">
+                        Ian.
+                      </a>
+                      I've tried my hand at gardening but I'm not quite there yet—my ferns have seen better days. I dislike the entire winter and summer seasons, waking up early, and IPAs.
+                    </p>
+                    <p>
+                      I studied music performance at Carnegie Mellon University, then took up wood working, began a Masters in Design + Technology at Parsons, and now I lead and manage full-stack engineers. I keep busy and I get things done. Check out
+                      <a href="/Ana_Enders_Resume.pdf" rel="noopener noreferrer" target="_blank">my resume</a>
+                      to learn more about what I do.
+                    </p>
+                  </div>
+                </div>           
+                <div class="resume">
+                  <h1>Software engineer</h1>
+                  <div class="resume-summary">
+                    I consider myself a generalist—shifting focus to whatever the business' needs are. I split my time between managing ten people and collaborating with the VP of Engineering and the CTO on behind-the-scences opperational fun. Having established our code standards for all our SASS, I review pull requests and work on features whenever I have bandwidth. When I'm not an individual contributor, or doing operational work, I dedicate my time to my direct reports, making sure they are happy, engaged, learning, and have what they need to be highly functional contributors.
+                  </div>
+                </div>
+              </div> 
+        
         );
     }
 }
--- src/pages/slideshow.js
+++ src/pages/slideshow.js
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
-import { Slide } from 'react-slideshow-image';
-import mald from '../images/maldive2.jpg';
+import { Fade } from 'react-slideshow-image';
+
 const slideImages = [
-  require('../images/maldive.jpg'),
-  require('../images/maldive1.jpg'),
-  require('../images/mald1.png'),
-  require('../images/slide_7.jpg'),
-  require('../images/slide_5.jpg'),
+  // require('../images/maldive.jpg'),
+  // require('../images/maldive1.jpg'),
+  // require('../images/mald1.png'),
+  // require('../images/slide_7.jpg'),
+  // require('../images/slide_5.jpg'),
+  require('../images/slide9.jpg'),
+  require('../images/slide4.jpg'),
+  require('../images/slide6.jpg'),
 ];
 const properties = {
   duration: 5000,
   transitionDuration: 500,
   infinite: true,
-  indicators: true,
-  arrows: true
+  indicators: false,
+  arrows:false
 }
 class slideshow extends Component {
     render() {
         return (
-            <Slide   {...properties }>
+            <Fade   {...properties }>
             {/* <div  className="each-slide">
             <img src={slideImages[0]} />
             </div>
             <div className="each-slide">
             <img src={slideImages[1]} />
             </div> */}
-            <div className="each-slide">             
-              <img src={slideImages[2]} />
+            <div className="each-fade">   
+            <div className="image-container">
+              <img alt="tour" src={slideImages[0]} />
+              </div>
             </div>
-            <div className="each-slide">             
-              <img src={slideImages[4]} />
+            <div className="each-fade">    
+            <div className="image-container">
+              <img alt="tour" src={slideImages[1]} />
+              </div>
             </div>
-          </Slide>
+            <div className="each-fade">    
+            <div className="image-container">
+              <img alt="tour" src={slideImages[2]} />
+              </div>
+            </div>
+          </Fade>
      
         );
     }
